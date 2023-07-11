51e3efc70b535a90f825a381c29e42dab99e4dd7
fabrimorat <fabrimorat@hotmail.com>
2023-07-11 14:41:59 -0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsumoAPIComponent } from './consumo-api/consumo-api.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'consumo-api', component: ConsumoAPIComponent },
+  // Otras rutas...
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
