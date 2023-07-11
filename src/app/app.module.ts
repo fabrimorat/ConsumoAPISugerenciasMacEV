fc8e4df72c1d9e11387009f20845b12d43eec9fc
fabrimorat <fabrimorat@hotmail.com>
2023-07-11 16:12:23 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConsumoAPIComponent } from './consumo-api/consumo-api.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ConsumoAPIComponent } from './consumo-api/consumo-api.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
